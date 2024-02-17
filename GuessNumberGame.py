dac601ead386d376bc349f796d6e5855c811a18c
tranduynam2904 <tranduynam2904@gmail.com>
2024-02-18 02:40:52 +0700

    18/02/2024

---
+++ GuessNumberGame.py
@@ -0,0 +1,38 @@
+# name = 'Nam'
+# age = 25
+#
+# print("Hello " + name + "!" + " You are " + str(age) + " years" + " old")
+import random
+import time
+
+while True:
+    # Create a random choice number
+    smallest_number = int(input("Enter the smallest number: "))
+    highest_number = int(input("Enter the highest number: "))
+    if smallest_number >= highest_number:
+        print(f"Smallest number must not be greater than highest number")
+    else:
+        break
+random_number = random.randint(smallest_number, highest_number)
+while True:
+        #Create a user choice number
+    # print(random_number)
+    user_number = int(input(f"Guess a number from {smallest_number} to {highest_number}: "))
+    if user_number < smallest_number or user_number > highest_number:
+        print(f"Number have to between {smallest_number} and {highest_number}")
+        time.sleep(0.5)
+        print(f"Please enter again!")
+        #Check the number
+    elif user_number < random_number:
+        smallest_number = user_number + 1
+        print(f"New Range from {smallest_number} to {highest_number}")
+    elif user_number > random_number:
+        highest_number = user_number - 1
+        print(f"New Range from {smallest_number} to {highest_number}")
+    else:
+        print(f"You win! It is {random_number}")
+        break
+    if smallest_number == highest_number:
+        print(f"You lose! It is {random_number}")
+        break
+
