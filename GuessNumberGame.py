e4003e4e41a61a7d0c5bed426bb72624e750090d
tranduynam2904 <tranduynam2904@gmail.com>
2024-02-19 02:33:51 +0700

    19/02/2024

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -1,39 +1,44 @@
-
 import random
 import time
+import PlayerMenu
+
+
 def guess_number_game():
+    PlayerMenu.player_menu()
+    number_of_players = PlayerMenu.execute_command()
     while True:
         # Create a random choice number
         smallest_number = int(input("Enter the smallest number: "))
         highest_number = int(input("Enter the highest number: "))
-        number_of_players = int(input("Enter the number of player: "))
         if smallest_number >= highest_number:
             print(f"Smallest number must not be greater than highest number")
         # elif number_of_players =
         else:
             break
     random_number = random.randint(smallest_number, highest_number)
-    while True:
-        # Create a user choice number
-        # print(random_number)
-        user_number = int(input(f"Guess a number from {smallest_number} to {highest_number}: "))
-        if user_number < smallest_number or user_number > highest_number:
-            print(f"Number have to between {smallest_number} and {highest_number}")
-            time.sleep(0.5)
-            print(f"Please enter again!")
-        # Check the number
-        elif user_number < random_number:
-            smallest_number = user_number + 1
-            print(f"New Range from {smallest_number} to {highest_number}")
-        elif user_number > random_number:
-            highest_number = user_number - 1
-            print(f"New Range from {smallest_number} to {highest_number}")
-        else:
-            print(f"You win! It is {random_number}")
-            break
-        if smallest_number == highest_number:
-            print(f"You lose! It is {random_number}")
-            break
+    for player in range(int(number_of_players)):
+        while True:
+            print(f"Player {player + 1}'s turn: ")
+            # Create a user choice number
+            # print(random_number)
+            user_number = int(input(f"Guess a number from {smallest_number} to {highest_number}: "))
+            if user_number < smallest_number or user_number > highest_number:
+                print(f"Number have to between {smallest_number} and {highest_number}")
+                time.sleep(0.5)
+                print(f"Please enter again!")
+            # Check the number
+            elif user_number < random_number:
+                smallest_number = user_number + 1
+                print(f"New Range from {smallest_number} to {highest_number}")
+            elif user_number > random_number:
+                highest_number = user_number - 1
+                print(f"New Range from {smallest_number} to {highest_number}")
+            else:
+                print(f"Player {player + 1} wins! It is {random_number}")
 
+            # Check if smallest = highest
+            if smallest_number == highest_number:
+                print(f"No one win! It is {random_number}")
+                break
 
 guess_number_game()
--- PlayerMenu.py
+++ PlayerMenu.py
@@ -1,21 +1,42 @@
 import os
+
+
 def player_menu():
     print("===== Player Menu =====")
-    print("1 Player Guess")
-    print("2 Player Guess")
-    print("3 Player Guess")
-    print("4 Player Guess")
-    print("Exit Game")
-def execute_command(choice):
+    print("1. 1 Player Guess")
+    print("2. 2 Player Guess")
+    print("3. 3 Player Guess")
+    print("4. 4 Player Guess")
+    print("5. Exit Game")
+
+
+def execute_command():
+    choice = input("Please select an option: ")
     if choice == '1':
-        os.system("start game with 1 Player Guess")
+        os.system("echo Starting game with 1 Player Guess")
     elif choice == '2':
-        os.system("start game with 2 Player Guess")
+        os.system("echo Starting game with 2 Player Guess")
     elif choice == '3':
-        os.system("start game with 3 Player Guess")
+        os.system("echo Starting game with 3 Player Guess")
     elif choice == '4':
-        os.system("start game with 4 Player Guess")
+        os.system("echo Starting game with 4 Player Guess")
     elif choice == '5':
         print("Exiting the game...")
+        exit()  # Exit the program
     else:
         print("Invalid choice. Please select a valid option.")
+        return None
+
+    return choice
+
+# def main():
+#     while True:
+#         player_menu()
+#         choice = input("Please select an option: ")
+#         execute_command(choice)
+#         if choice == '5':
+#             break  # Thoát khỏi vòng lặp khi người dùng chọn thoát
+#
+#
+# if __name__ == "__main__":
+#     main()
