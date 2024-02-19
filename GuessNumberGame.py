bbe3d65b5d44dacd5c6eaf9ee4379d4475a08608
tranduynam2904 <tranduynam2904@gmail.com>
2024-02-20 02:24:52 +0700

    20/02/2024

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -6,39 +6,39 @@ import PlayerMenu
 def guess_number_game():
     PlayerMenu.player_menu()
     number_of_players = PlayerMenu.execute_command()
-    while True:
-        # Create a random choice number
+
+    if number_of_players:
+        print("\nPlayer 1, please set up the number range")
+    # Create a limit number
+    smallest_number = int(input("Enter the smallest number: "))
+    highest_number = int(input("Enter the highest number: "))
+    while smallest_number >= highest_number:
+        print("Smallest number must not be greater than highest number")
+        print("Please enter again!")
         smallest_number = int(input("Enter the smallest number: "))
         highest_number = int(input("Enter the highest number: "))
-        if smallest_number >= highest_number:
-            print(f"Smallest number must not be greater than highest number")
-        # elif number_of_players =
-        else:
-            break
     random_number = random.randint(smallest_number, highest_number)
-    for player in range(int(number_of_players)):
-        while True:
-            print(f"Player {player + 1}'s turn: ")
-            # Create a user choice number
-            # print(random_number)
+    while True:
+        for player in range(1, int(number_of_players) + 1):
+            print(f"Player {player}'s turn: ")
             user_number = int(input(f"Guess a number from {smallest_number} to {highest_number}: "))
             if user_number < smallest_number or user_number > highest_number:
-                print(f"Number have to between {smallest_number} and {highest_number}")
+                print(f"Number have to be between {smallest_number} and {highest_number}")
                 time.sleep(0.5)
-                print(f"Please enter again!")
-            # Check the number
+                print("Please enter again!")
             elif user_number < random_number:
                 smallest_number = user_number + 1
-                print(f"New Range from {smallest_number} to {highest_number}")
+                print(f"\nNew Range from {smallest_number} to {highest_number}")
             elif user_number > random_number:
                 highest_number = user_number - 1
-                print(f"New Range from {smallest_number} to {highest_number}")
+                print(f"\nNew Range from {smallest_number} to {highest_number}")
             else:
-                print(f"Player {player + 1} wins! It is {random_number}")
+                print(f"\nPlayer {player} wins! It is {random_number}")
+                return  # End the game when someone win
 
-            # Check if smallest = highest
-            if smallest_number == highest_number:
-                print(f"No one win! It is {random_number}")
-                break
+            if smallest_number >= highest_number:
+                print(f"No one wins this round! The number was {random_number}")
+                break  # Break the loop
+
 
 guess_number_game()
